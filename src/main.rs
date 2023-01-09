096ed5b8c53200a61785b2fe28ced3f76e1956cd
Evryon <fireden70@gmail.com>
2023-01-10 00:39:44 +0100

    wtf is shl

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,14 @@ static HELLO: &[u8] = b"THIS IS INSANE!";
 // reminder of what mangling is: https://en.wikipedia.org/wiki/Name_mangling
 #[no_mangle] // Dont mangle the function name
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8;
-
-    // enumerate() returns couples consisting of the byte representing the character and its index in the array
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
-        }
-    }
-
+    write("hola");
     loop {}
 }
 
 // Panic function
 use core::panic::PanicInfo;
+use crate::vga_buffer::write;
+
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,6 +1,9 @@
+use core::ops::Shl;
+use crate::vga_buffer::Color::{Black, Red, Yellow};
+
 #[allow(dead_code)]
 #[derive(PartialEq, Eq, Copy, Clone, Debug)]
-#[repr(u8)]
+#[repr(u8)] // Displays memory as u8
 pub enum Color {
     Black = 0,
     Blue = 1,
@@ -25,4 +28,61 @@ impl ColorCode {
     fn new(background: Color, foreground: Color) -> ColorCode {
         ColorCode(background << 4 | foreground)
     }
+}
+
+struct VgaChar {
+    character: u8,
+    color: ColorCode
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+struct Buffer {
+    chars: [[VgaChar; BUFFER_HEIGHT]; BUFFER_WIDTH]
+}
+
+pub struct Writer {
+    column: usize,
+    color: ColorCode,
+    buffer: &'static mut Buffer
+}
+impl Writer {
+    pub fn write(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe)
+            }
+        }
+    }
+
+    pub fn write_byte(&mut self, byte: u8) {
+        if byte == b'\n' {
+            self.new_line();
+        } else {
+            if self.column > BUFFER_WIDTH {
+                self.new_line();
+            }
+            let row = BUFFER_HEIGHT - 1;
+            let col = self.column;
+            self.buffer.chars[row][col] = VgaChar {
+                character: byte,
+                color: self.color,
+            };
+            self.column += 1;
+        }
+    }
+    fn new_line(&mut self) {
+        // TODO: Add new line
+    }
+}
+
+pub fn write(s: &str) {
+    let mut writer = Writer {
+        column: 0,
+        color: ColorCode::new(Red, Black),
+        buffer: unsafe {&mut *(0xb8000 as *mut Buffer)}
+    };
+    writer.write(s)
 }
