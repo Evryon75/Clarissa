4d7b5a564a744ce8862ad507b65447e15d46a982
Evryon <fireden70@gmail.com>
2023-02-22 23:16:57 +0100

    the funyn bro i had to bring my old home to a new world in terraria because funny calamity update xdd!! it was a tree house it was one of those triple tree house setups bro it was so big jesus christ and i was like almost done with prehardmode aswell and then the pylons bro the pylons asldkasdò

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -6,8 +6,10 @@ use lazy_static::lazy_static;
 use pc_keyboard::{layouts, HandleControl, ScancodeSet1, DecodedKey};
 use pic8259::ChainedPics;
 use spin::Mutex;
+use x86_64::instructions::hlt;
 use x86_64::instructions::port::Port;
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::registers::control::Cr2;
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 pub fn initialize_idt() {
     IDT.load();
@@ -17,6 +19,7 @@ lazy_static! {
     static ref IDT: InterruptDescriptorTable = {
         let mut idt = InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt[InterruptIndex::Timer.as_usize()].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard.as_usize()].set_handler_fn(keyboard_interrupt_handler);
         unsafe {
@@ -34,6 +37,13 @@ pub const PIC_2_OFFSET: u8 = 40;
 pub static PICS: Mutex<ChainedPics> =
     Mutex::new(unsafe { ChainedPics::new(PIC_1_OFFSET, PIC_2_OFFSET) });
 
+extern "x86-interrupt" fn page_fault_handler(stack: InterruptStackFrame, error: PageFaultErrorCode) {
+    lightredln!("Exception: Page Fault");
+    lightredln!("Accessed address: {:?}", Cr2::read());
+    lightredln!("Accessed code: {:?}", error);
+    lightredln!("Stack frame: {:#?}", stack);
+    hlt();
+}
 extern "x86-interrupt" fn keyboard_interrupt_handler(_stack: InterruptStackFrame) {
     use pc_keyboard::Keyboard;
 
